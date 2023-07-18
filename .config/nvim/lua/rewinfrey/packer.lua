4f9924e9075365d0deb960e68dcfa1dd8df22de0
Rick Winfrey <rewinfrey@github.com>
2023-07-17 21:45:01 -0700

    Add leap plugin

--- .config/nvim/lua/rewinfrey/packer.lua
+++ .config/nvim/lua/rewinfrey/packer.lua
@@ -28,6 +28,10 @@ require('packer').startup({
 			},
 		}
 
+		use {
+			'ggandor/leap.nvim',
+		}
+
 		use { -- Autocompletion
 			'hrsh7th/nvim-cmp',
 			requires = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
