2d34073a16985a879df198f70eeb1d0d530fd3a0
Rick Winfrey <rewinfrey@github.com>
2023-07-17 17:41:01 -0700

    Temporarily comment out fidget.nvim

--- .config/nvim/lua/rewinfrey/packer.lua
+++ .config/nvim/lua/rewinfrey/packer.lua
@@ -21,7 +21,7 @@ require('packer').startup({
 				'williamboman/mason-lspconfig.nvim',
 
 				-- Useful status updates for LSP
-				'j-hui/fidget.nvim',
+				-- 'j-hui/fidget.nvim',
 
 				-- Additional lua configuration, makes nvim stuff amazing
 				'folke/neodev.nvim',
