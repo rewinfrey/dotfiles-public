ecd8a10f53c8c5a85a7295df7222c261e70363ed
Rick Winfrey <rick.winfrey@gmail.com>
2025-07-11 23:34:41 -0700

    Temporarily move after plugin lua config to back

--- .config/nvim/after-back/plugin/telescope.lua
+++ .config/nvim/after-back/plugin/telescope.lua
@@ -44,8 +44,8 @@ M.project_files = function()
 end
 
 -- See `:help telescope.builtin`
-vim.keymap.set('n', '<leader>so', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader>sb', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader>so', require('telescope.builtin').oldfiles, { desc = '[?] [S]earch [O]ld files' })
+vim.keymap.set('n', '<leader>sb', require('telescope.builtin').buffers, { desc = '[ ] [S]earch [B]uffers' })
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sp', require('telescope.builtin').git_files, { desc = '[S]earch [P]roject' })
 vim.keymap.set('n', '<leader>p', M.project_files, { desc = '[S]earch [P]roject files' })
@@ -54,7 +54,7 @@ vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { de
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>st', require('telescope.builtin').treesitter, { desc = '[S]earch [T]ree-sitter' })
-vim.keymap.set('n', '<leader>sb', require('telescope.builtin').builtin, { desc = '[S]earch [B]uiltin' })
+vim.keymap.set('n', '<leader>si', require('telescope.builtin').builtin, { desc = '[S]earch [B]uiltin' })
 vim.keymap.set('n', '<leader>gd', require('telescope.builtin').lsp_definitions, { desc = '[G]oto [D]efinition' })
 vim.keymap.set('n', '<leader>gr', require('telescope.builtin').lsp_references, { desc = '[G]oto [R]eferences' })
 vim.keymap.set('n', '<leader>/', function()
